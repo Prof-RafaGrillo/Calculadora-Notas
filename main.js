81085668d29ff6bc832a2a7b538bdb2cce8e6832
Rafael <ra.grillo@gmail.com>
2024-09-15 21:24:23 -0300

    Adicionando o card

--- main.js
+++ main.js
@@ -15,12 +15,35 @@ function adicionarDisciplina (){
     n1 = form.primeira.value
     n2 = form.segunda.value
 
-    soma = n1 + n2
+
+    soma =parseFloat(n1) + parseFloat(n2)
+    
     media = (soma)/2
 
+    faltante = 18 - soma
+
+    porcentagem = ((soma * 100) / 18).toFixed(1)
+
     const cards = document.querySelector(".cards")
-    cards.innerHTML += `<p> Teste </p>`
-    cards.innerHTML += `<p> Teste </p>`
+
+    const card = document.querySelector(".card")
+    cards.innerHTML += `
+        <div class="card">
+                <div class="card-notas">
+                    <h4>${disciplina}</h4>
+                    <h5>${n1}</h5>
+                    <h5>${n2}</h5>
+                </div>
+                <div class="barra">
+                    <div class="progresso--bar" style="width:${porcentagem}%"></div>
+                    <div class="barra--valor" >${porcentagem}%</div>
+                </div>
+                <p class="card--resultado">Você precisa de uma nota ${faltante} no 3 trimestre</p>
+                <p class="card--resultado">Falta pouco para passar!</p>
+            </div>
+    `
+
+
 
     // const card = document.createElement('div')
     // const conteudo = document.createTextNode( `${disciplina} + ${n1}`)
@@ -30,3 +53,4 @@ function adicionarDisciplina (){
    
   
 }
+
